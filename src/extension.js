98baec0cabfde6d49240ceacc43c282db32da8a0
wotmshuaisi <16408269+wotmshuaisi@users.noreply.github.com>
2019-09-06 03:19:32 +0100

    follow gnome extension requirements for geting approval

--- src/extension.js
+++ src/extension.js
@@ -9,24 +9,9 @@ const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
 const Convenience = Me.imports.convenience;
 
-// for application - constants
-const _httpSession = new Soup.SessionAsync();
-const api_endpoint = "https://data-asg.goldprice.org/GetData/";
-const WEIGHT_UNIT = "weight-uint";
-const CURRENCY = "currency";
-const REFRESH_INTERVAL = "refresh-interval";
-const WEIGHT_OPTIONS = {
-    0: "℥",
-    1: "g",
-    2: "kg",
-}
 // for application - variables
-let text, button, price, settings, taskLock, refresh_trigger, pause;
-
-function set_headers(request) {
-    request.request_headers.append('cache-control', "no-cache");
-    request.request_headers.append('user-agent', "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3833.120 Safari/537.36");
-}
+let _httpSession, CURRENCY, WEIGHT_UNIT, REFRESH_INTERVAL, WEIGHT_OPTIONS;
+let price_label, settings, taskLock, pause;
 
 function _refresh_price() {
     // lock
@@ -35,8 +20,9 @@ function _refresh_price() {
     }
     taskLock = true
     // request
-    let request = Soup.Message.new("GET", api_endpoint + settings.get_string(CURRENCY) + "-XAU/1");
-    set_headers(request);
+    request = Soup.Message.new("GET", "https://data-asg.goldprice.org/GetData/" + settings.get_string("currency") + "-XAU/1");
+    request.request_headers.append('cache-control', "no-cache");
+    request.request_headers.append('user-agent', "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3833.120 Safari/537.36");
     _httpSession.queue_message(request, function (session, message) {
         if (request.status_code !== 200) {
             log("[Gold Price Monitor]: bad response - ", message.status_code);
@@ -58,8 +44,8 @@ function _refresh_price() {
                 price_number = price_number / 31.1034768 * 1000;
                 break;
         }
-        log("[Gold Price Monitor]: " + price.text + " => " + price_number.toFixed(3));
-        price.text = price_number.toFixed(3) + "(" + settings.get_string(CURRENCY) + ") / " + WEIGHT_OPTIONS[settings.get_int(WEIGHT_UNIT)];
+        log("[Gold Price Monitor]: " + price_label.text + " => " + price_number.toFixed(3));
+        price_label.text = price_number.toFixed(3) + "(" + settings.get_string(CURRENCY) + ") / " + WEIGHT_OPTIONS[settings.get_int(WEIGHT_UNIT)];
     });
 
     taskLock = false;
@@ -70,6 +56,21 @@ function _refresh_price() {
 }
 
 function init() {
+}
+
+function enable() {
+    // variable initializing
+    pause = false;
+    _httpSession = new Soup.SessionAsync();
+    WEIGHT_UNIT = "weight-uint";
+    CURRENCY = "currency";
+    REFRESH_INTERVAL = "refresh-interval";
+    WEIGHT_OPTIONS = {
+        0: "℥",
+        1: "g",
+        2: "kg",
+    }
+
     // settings
     settings = Convenience.getSettings();
     // Widget
@@ -81,21 +82,31 @@ function init() {
         y_fill: false,
         track_hover: true
     });
-    price = new St.Label({
+    price_label = new St.Label({
         text: "Loading..."
     });
-    button.set_child(price);
-    // Events
+    button.set_child(price_label);
+    // Event Looping
     button.connect('button-press-event', _refresh_price);
     Mainloop.timeout_add_seconds(1, Lang.bind(this, _refresh_price));
-}
 
-function enable() {
-    pause = false;
     Main.panel._rightBox.insert_child_at_index(button, 0);
+
 }
 
 function disable() {
     pause = true;
     Main.panel._rightBox.remove_child(button);
+
+    delete _httpSession;
+    delete CURRENCY;
+    delete WEIGHT_UNIT;
+    delete REFRESH_INTERVAL;
+    delete WEIGHT_OPTIONS;
+
+    delete pause;
+    delete price_label;
+    delete settings;
+    delete taskLock;
+
 }
