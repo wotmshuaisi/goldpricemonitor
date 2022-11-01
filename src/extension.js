6aac5f75e5c7b36e484fb539a6588dbd8471083e
wotmshuaisi <16408269+wotmshuaisi@users.noreply.github.com>
2022-11-01 22:11:25 +0800

    Support Gnome 43

--- src/extension.js
+++ src/extension.js
@@ -18,7 +18,7 @@
 
 /* exported init */
 
-const GETTEXT_DOMAIN = 'goldprice-indicator-extension';
+const GETTEXT_DOMAIN = "goldprice-indicator-extension";
 
 const { GObject, St, Gtk, Clutter } = imports.gi;
 const Gio = imports.gi.Gio;
@@ -36,181 +36,233 @@ const Mainloop = imports.mainloop;
 const GLib = imports.gi.GLib;
 
 const GoldPriceIndicator = GObject.registerClass(
-    class GoldPriceIndicator extends PanelMenu.Button {
+  class GoldPriceIndicator extends PanelMenu.Button {
+    _init() {
+      super._init(0.0, _("GOldPriceIndicatorButton"));
+      this.periodic_task;
+      this.lock = false;
+      this.settings;
+      this.extension_icon;
+      this.price;
+      this.lastUpdate;
+      this.menu;
+      this.api_url = "https://data-asg.goldprice.org/GetData/";
+      this.WEIGHT_OPTIONS = {
+        0: "℥",
+        1: "g",
+        2: "kg",
+      };
+      this._httpSession = new Soup.Session();
 
-        _init() {
-            super._init(0.0, _('GOldPriceIndicatorButton'));
-            this.periodic_task;
-            this.lock = false;
-            this.settings;
-            this.extension_icon;
-            this.price;
-            this.lastUpdate;
-            this.menu;
-            this.api_url = 'https://data-asg.goldprice.org/GetData/';
-            this.WEIGHT_OPTIONS = {
-                0: "℥",
-                1: "g",
-                2: "kg",
-            };
-            this._httpSession = new Soup.SessionAsync();
+      let gschema = Gio.SettingsSchemaSource.new_from_directory(
+        Me.dir.get_child("schemas").get_path(),
+        Gio.SettingsSchemaSource.get_default(),
+        false
+      );
+      // settings
+      this.settings = new Gio.Settings({
+        settings_schema: gschema.lookup(
+          "org.gnome.shell.extensions.gold-price-monitor",
+          true
+        ),
+      });
 
+      // Indicator
+      let box = new St.BoxLayout({ style_class: "panel-status-menu-box" });
 
-            let gschema = Gio.SettingsSchemaSource.new_from_directory(
-                Me.dir.get_child('schemas').get_path(),
-                Gio.SettingsSchemaSource.get_default(),
-                false
-            );
-            // settings
-            this.settings = new Gio.Settings({
-                settings_schema: gschema.lookup('org.gnome.shell.extensions.gold-price-monitor', true)
-            });
+      // Icon + Label
+      // this.extension_icon = new St.Icon({
+      //     // icon_name: 'face-smile-symbolic',
+      //     icon_name: 'content-loading-symbolic',
+      //     style_class: 'system-status-icon',
+      // });
+      this.price = new St.Label({
+        text: "...",
+        y_align: Clutter.ActorAlign.CENTER,
+      });
 
-            // Indicator
-            let box = new St.BoxLayout({ style_class: 'panel-status-menu-box' });
+      // drop-down menu
+      this.lastUpdate = new PopupMenu.PopupMenuItem(_("Last update:"));
+      let refresh = new PopupMenu.PopupMenuItem(_("Refresh"));
 
-            // Icon + Label
-            // this.extension_icon = new St.Icon({
-            //     // icon_name: 'face-smile-symbolic',
-            //     icon_name: 'content-loading-symbolic',
-            //     style_class: 'system-status-icon',
-            // });
-            this.price = new St.Label({
-                text: '...',
-                y_align: Clutter.ActorAlign.CENTER,
-            });
+      refresh.connect("activate", () => {
+        this.refreshData();
+      });
 
-            // drop-down menu
-            this.lastUpdate = new PopupMenu.PopupMenuItem(_('Last update:'));
-            let refresh = new PopupMenu.PopupMenuItem(_('Refresh'));
+      // set widgets
+      this.menu.addMenuItem(this.lastUpdate);
+      this.menu.addMenuItem(refresh);
 
-            refresh.connect('activate', () => {
-                this.refreshData();
-            });
+      // box.add_child(this.extension_icon);
+      box.add_child(this.price);
+      box.add_child(PopupMenu.arrowIcon(St.Side.BOTTOM));
+      this.add_child(box);
 
-            // set widgets
-            this.menu.addMenuItem(this.lastUpdate);
-            this.menu.addMenuItem(refresh);
+      this.refreshData();
+    }
 
-            // box.add_child(this.extension_icon);
-            box.add_child(this.price);
-            box.add_child(PopupMenu.arrowIcon(St.Side.BOTTOM));
-            this.add_child(box);
+    buildRequest() {
+      const url =
+        this.api_url + this.settings.get_value("currency").unpack() + "-XAU/1";
+      let request = Soup.Message.new("GET", url);
+      request.request_headers.append("Cache-Control", "no-cache");
+      request.request_headers.append(
+        "User-Agent",
+        "Mozilla/5.0 (Windows NT 6.3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.84 Safari/537.36"
+      );
 
-            this.refreshData();
+      // this.log([url]); // debug
+      return request;
+    }
 
+    refreshData() {
+      if (this.lock) {
+        return;
+      }
+      this.lock = true;
+      let msg = this.buildRequest();
+      this._httpSession.send_and_read_async(
+        msg,
+        GLib.PRIORITY_DEFAULT,
+        null,
+        (_, response) => {
+          response = new TextDecoder("utf-8").decode(
+            this._httpSession.send_and_read_finish(response).get_data()
+          );
+
+          if (msg.get_status() > 299) {
+            this.log(["Remote server error:", msg.get_status(), response]);
+            return;
+          }
+
+          const json_data = JSON.parse(response);
+          if (json_data.length === 0) {
+            this.log(["Remote server error:", response]);
+            return;
+          }
+
+          let latest_price = Number.parseFloat(json_data[0].split(",")[1]);
+          switch (this.settings.get_value("weight-uint").unpack()) {
+            case 1:
+              latest_price = latest_price / 31.1034768;
+              break;
+            case 2:
+              latest_price = (latest_price / 31.1034768) * 1000;
+              break;
+          }
+
+          latest_price = latest_price.toFixed(3);
+          if (!this.settings.get_value("hide-unit").unpack()) {
+            latest_price +=
+              "(" +
+              this.settings.get_value("currency").unpack() +
+              ") / " +
+              this.WEIGHT_OPTIONS[
+                this.settings.get_value("weight-uint").unpack()
+              ];
+          }
+
+          this.log([
+            `Update price from: ${this.price.text} to ${latest_price}`,
+          ]);
+
+          this.price.text = latest_price;
+
+          this.lastUpdate.label_actor.text =
+            "Last update: " + new Date().toLocaleTimeString();
         }
-
-        buildRequest() {
-            const url = this.api_url + this.settings.get_value('currency').unpack() + '-XAU/1';
-            let request = Soup.Message.new('GET', url);
-            request.request_headers.append('Cache-Control', 'no-cache');
-            request.request_headers.append('User-Agent', 'Mozilla/5.0 (Windows NT 6.3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.84 Safari/537.36');
-
-            this.log([url]); // debug
-            return request;
+      );
+      this.lock = false;
+      this.purgeBackgroundTask();
+      this.periodic_task = Mainloop.timeout_add_seconds(
+        this.settings.get_value("refresh-interval").unpack() * 60,
+        () => {
+          this, this.refreshData;
         }
+      );
+    }
 
-        refreshData() {
-            if (this.lock) {
-                return;
-            }
-            this.lock = true;
-            this._httpSession.queue_message(this.buildRequest(), (_, response) => {
-                if (response.status_code > 299) {
-                    this.log(['Remote server error:', response.status_code, response.response_body.data]);
-                    return;
-                }
-                const json_data = JSON.parse(response.response_body.data);
-                if (json_data.length === 0) {
-                    this.log(['Remote server error:', response.response_body.data]);
-                    return;
-                }
+    log(logs) {
+      global.log("[GoldPriceMonitor]", logs.join(", "));
+    }
 
-                let latest_price = Number.parseFloat(json_data[0].split(",")[1]);
-                switch (this.settings.get_value('weight-uint').unpack()) {
-                    case 1:
-                        latest_price = latest_price / 31.1034768;
-                        break;
-                    case 2:
-                        latest_price = latest_price / 31.1034768 * 1000;
-                        break;
-                }
-
-                latest_price = latest_price.toFixed(3);
-                if (!this.settings.get_value('hide-unit').unpack()) {
-                    latest_price += "(" + this.settings.get_value('currency').unpack() + ") / " + this.WEIGHT_OPTIONS[this.settings.get_value('weight-uint').unpack()];
-                }
-
-                this.log([`Update price from: ${this.price.text} to ${latest_price}`]);
-
-                this.price.text = latest_price;
-
-                this.lastUpdate.label_actor.text = 'Last update: ' + new Date().toLocaleTimeString();;
-            });
-            this.lock = false;
-            this.purgeBackgroundTask();
-            this.periodic_task = Mainloop.timeout_add_seconds(this.settings.get_value('refresh-interval').unpack() * 60, () => { this, this.refreshData });
-        }
-
-        log(logs) {
-            global.log('[GoldPriceMonitor]', logs.join(', '));
-        }
-
-        purgeBackgroundTask() {
-            if (this.periodic_task) {
-                GLib.Source.remove(this.periodic_task);
-                this.periodic_task = null;
-            }
-        }
-
-    });
+    purgeBackgroundTask() {
+      if (this.periodic_task) {
+        GLib.Source.remove(this.periodic_task);
+        this.periodic_task = null;
+      }
+    }
+  }
+);
 
 class Extension {
-    constructor(uuid) {
-        this._uuid = uuid;
-        ExtensionUtils.initTranslations(GETTEXT_DOMAIN);
+  constructor(uuid) {
+    this._uuid = uuid;
+    ExtensionUtils.initTranslations(GETTEXT_DOMAIN);
+  }
+
+  enable() {
+    this._indicator = new GoldPriceIndicator();
+
+    const indicator_position = this._indicator.settings
+      .get_value("panel-position")
+      .unpack();
+    if (indicator_position === "Left") {
+      Main.panel.addToStatusArea(
+        this._uuid,
+        this._indicator,
+        Main.panel._leftBox.get_children().length,
+        "left"
+      );
+    } else if (indicator_position === "Center") {
+      Main.panel.addToStatusArea(
+        this._uuid,
+        this._indicator,
+        Main.panel._centerBox.get_children().length,
+        "center"
+      );
+    } else {
+      Main.panel.addToStatusArea(this._uuid, this._indicator);
     }
 
-    enable() {
-        this._indicator = new GoldPriceIndicator();
+    this._indicator.settings.connect("changed::panel-position", () => {
+      this.addToPanel();
+    });
+  }
 
-        const indicator_position = this._indicator.settings.get_value('panel-position').unpack()
-        if (indicator_position === 'Left') {
-            Main.panel.addToStatusArea(this._uuid, this._indicator, Main.panel._leftBox.get_children().length, 'left');
-        } else if (indicator_position === 'Center') {
-            Main.panel.addToStatusArea(this._uuid, this._indicator, Main.panel._centerBox.get_children().length, 'center');
-        } else {
-            Main.panel.addToStatusArea(this._uuid, this._indicator);
-        }
+  disable() {
+    this._indicator.purgeBackgroundTask();
+    this._indicator.destroy();
+    this._indicator = null;
+  }
 
-        this._indicator.settings.connect('changed::panel-position', () => { this.addToPanel() });
-
+  addToPanel() {
+    this._indicator.destroy();
+    this._indicator = null;
+    this._indicator = new GoldPriceIndicator();
+    const indicator_position = this._indicator.settings
+      .get_value("panel-position")
+      .unpack();
+    if (indicator_position === "Left") {
+      Main.panel.addToStatusArea(
+        this._uuid,
+        this._indicator,
+        Main.panel._leftBox.get_children().length,
+        "left"
+      );
+    } else if (indicator_position === "Center") {
+      Main.panel.addToStatusArea(
+        this._uuid,
+        this._indicator,
+        Main.panel._centerBox.get_children().length,
+        "center"
+      );
+    } else {
+      Main.panel.addToStatusArea(this._uuid, this._indicator);
     }
-
-    disable() {
-        this._indicator.purgeBackgroundTask();
-        this._indicator.destroy();
-        this._indicator = null;
-    }
-
-
-    addToPanel() {
-        this._indicator.destroy();
-        this._indicator = null;
-        this._indicator = new GoldPriceIndicator();
-        const indicator_position = this._indicator.settings.get_value('panel-position').unpack()
-        if (indicator_position === 'Left') {
-            Main.panel.addToStatusArea(this._uuid, this._indicator, Main.panel._leftBox.get_children().length, 'left');
-        } else if (indicator_position === 'Center') {
-            Main.panel.addToStatusArea(this._uuid, this._indicator, Main.panel._centerBox.get_children().length, 'center');
-        } else {
-            Main.panel.addToStatusArea(this._uuid, this._indicator);
-        }
-    }
-
+  }
 }
 
 function init(meta) {
-    return new Extension(meta.uuid);
+  return new Extension(meta.uuid);
 }
